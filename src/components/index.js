d2ef2ff474c24889b2046513aa821ca888e34ef7
Runar <runarnazmutdinov@gmail.com>
2022-09-24 14:42:13 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import './header/header';
+import './footer/footer';
+import './hero/hero';
+import './process/process';
+import './benefits/benefits';
+import './consult/consult';
+import './video/video';
+import './gallery/gallery';
